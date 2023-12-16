1bde63e8ee796cfa390227427169eeb522816644
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-16 17:40:15 +0100

    learning javascript

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 // user input
 // alert("Hello world!");
+/* let myBoolean = confirm("Ok === True\nCancel === False");
+console.log(myBoolean); */
+let myName = prompt("Please enter your name.");
+console.log(myName);
