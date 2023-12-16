3992bc4b17ddbddd7c249310c460f4607126505d
OLAGUNJU Oladele Emmanuel <149441709+MLO-OLAGUNJU@users.noreply.github.com>
2023-12-16 22:05:24 +0100

    learning javascript

--- js/main.js
+++ js/main.js
@@ -5,8 +5,10 @@ console.log(myBoolean); */
 let myName = prompt("Please enter your name.");
 // console.log(typeof myName);
 
-if (myName.length) {
-  console.log(myName ?? "You didn't enter your name.");
+if (myName) {
+  console.log(myName.length);
+  console.log(myName.trim().length);
+  console.log(myName.trim());
 } else {
   console.log("You didn't enter your name");
 }
